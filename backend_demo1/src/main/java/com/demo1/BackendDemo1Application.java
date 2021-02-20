69afa0bc519c9f72155f02add4d53201357d2021
G Govardhan Reddy <ggovardhan6495@gmail.com>
2021-02-20 14:11:50 +0530

    first commit

---
+++ backend_demo1/src/main/java/com/demo1/BackendDemo1Application.java
@@ -0,0 +1,13 @@
+package com.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDemo1Application.class, args);
+	}
+
+}
